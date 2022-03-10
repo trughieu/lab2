9f161bd41c3ca3fc257c3fb66d8cd1a81e0706e9
Híu Nè <75436344+trughieu@users.noreply.github.com>
2022-03-10 22:53:58 +0700

    First commit

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -7,6 +7,13 @@ namespace lab2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int a, b, sum;
+            a = 4;
+            b = 5;
+            sum = a + b;
+            Console.WriteLine(sum);
+          
         }
     }
 }
